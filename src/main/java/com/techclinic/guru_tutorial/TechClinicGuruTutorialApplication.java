e72603e86ba3eb97cd47ed6e54a10cc284a1b91d
zaparas <pasx.zap@gmail.com>
2022-07-07 09:34:41 +0300

    1rst Commit.
    Changes: Spring Initialzr product. 1rst run perofrmed.

---
+++ src/main/java/com/techclinic/guru_tutorial/TechClinicGuruTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.techclinic.guru_tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechClinicGuruTutorialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechClinicGuruTutorialApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/techclinic/guru_tutorial/TechClinicGuruTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techclinic.guru_tutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechClinicGuruTutorialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
